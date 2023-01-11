207cf8b06963188c67df7a18a516b5ff62c4d840
JinEunPark <wlsdmsrnfl@naver.com>
2023-01-12 00:13:18 +0900

    [2022-01-11]root dir 변경함

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/app"
+PROJECT_ROOT="/home/ubuntu/Fachive_Porject_Backend_main"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
