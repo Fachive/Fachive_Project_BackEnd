6d0beb6ba5178a59d9b69d868253de29cc886284
JinEunPark <wlsdmsrnfl@naver.com>
2023-01-12 14:00:08 +0900

    [2022-01-12] CICD 구축 성공 설정 절데 건드리지 말것

--- script/start.sh
+++ script/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/Fachive_Porject_Backend_main"
-JAR_FILE="$PROJECT_ROOT/backend-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/Fachive_Porject_Backend_main-0.0.1-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
--- script/stop.sh
+++ script/stop.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/Fachive_Porject_Backend_main"
-JAR_FILE="$PROJECT_ROOT/backend-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/Fachive_Porject_Backend_main-0.0.1-SNAPSHOT.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
