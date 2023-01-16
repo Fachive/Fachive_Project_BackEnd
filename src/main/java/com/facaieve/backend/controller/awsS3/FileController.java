d6214790e62b42e6bc4075bb561cc87b227bb5a1
JinEunPark <wlsdmsrnfl@naver.com>
2023-01-16 20:54:46 +0900

    [2022-01-16] file 저장 api 생성함

---
+++ src/main/java/com/facaieve/backend/controller/awsS3/FileController.java
@@ -0,0 +1,43 @@
+package com.facaieve.backend.controller.awsS3;
+
+import com.facaieve.backend.service.aswS3.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/files")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class FileController {
+
+    private static final String MESSAGE_1 = "Uploaded the file successfully";
+    private static final String FILE_NAME = "fileName";
+
+    @Autowired
+    FileService fileService;
+
+    @GetMapping
+    public ResponseEntity<Resource> findByName(@RequestBody(required = false) Map<String, String> params) {
+        return ResponseEntity
+                .ok()
+                .cacheControl(CacheControl.noCache())
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + params.get(FILE_NAME) + "\"")
+                .body(new InputStreamResource(fileService.findByName(params.get(FILE_NAME))));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestParam("file") MultipartFile multipartFile) {
+        fileService.save(multipartFile);
+        return new ResponseEntity<>(MESSAGE_1, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/facaieve/backend/service/aswS3/UploadService.java
@@ -0,0 +1,12 @@
+package com.facaieve.backend.service.aswS3;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
+import java.io.InputStream;
+
+public interface UploadService {
+
+    void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName);
+
+    String getFileUrl(String fileName);
+}
