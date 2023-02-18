f2464129e3922eb6dc28aa56f215b20930625c13
Akkuchan <pohd8494@naver.com>
2023-02-18 15:36:25 +0900

    fix/토큰값 베어러 부분 수정2

--- src/main/java/com/facaieve/backend/service/etc/CategoryService.java
+++ src/main/java/com/facaieve/backend/service/etc/CategoryService.java
@@ -51,13 +51,11 @@ public class CategoryService {
 
     public CategoryEntity getCategory(CategoryEntity categoryEntity){
 
-        boolean test = categoryRepository.existsByCategoryName(categoryEntity.getCategoryName());
-
         if(categoryEntity.getCategoryName().equals("total")){
             log.info("전체 카테고리로 검색합니다. {}", categoryEntity);
             return CategoryEntity.builder().categoryName("total").build();
         }
-        else if(test){
+        else if(categoryRepository.existsByCategoryName(categoryEntity.getCategoryName())){
             return categoryRepository.findCategoryEntityByCategoryName(categoryEntity.getCategoryName()).orElseThrow(()
                     -> new BusinessLogicException(ExceptionCode.NO_SUCH_CATEGORY));
         }else{
